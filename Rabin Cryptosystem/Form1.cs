c381348f2640e1a9a54e7e850d2dd701cc082259
kosuev <kosuev.o@gmail.com>
2016-10-08 23:43:08 +0300

    Add new form and prime function

---
+++ Rabin Cryptosystem/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rabin_Cryptosystem
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        bool prime(int n)
+        {
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+                if (n % i == 0)
+                    return false;
+            return true;
+        }
+    }
+}
