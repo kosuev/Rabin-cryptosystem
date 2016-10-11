956096eb4262347ec2d2d3ad7a60f63c40089a02
kosuev <kosuev.o@gmail.com>
2016-10-11 20:14:08 +0300

    China function - ok. Finish.

--- Rabin Cryptosystem/Form1.cs
+++ Rabin Cryptosystem/Form1.cs
@@ -27,22 +27,20 @@ namespace Rabin_Cryptosystem
                 p = rand.Next(1, 99);
                 q = rand.Next(1, 99);
             }
-            while ((p%4!=3) || (q%4!=3)||(prime(p)!=true)||(prime(q)!=true)|| (p * q < 255));
+            while ((p%4!=3) || (q%4!=3)||(prime(p)!=true)||(prime(q)!=true));
             int n = p * q;
 
             //encrypt
             string shifr = null;            
             string opentxt = OpentxtBox.Text;
             for (int i = 0; i < opentxt.Length; i++)
-            {
+            {              
                 shifr = shifr + Convert.ToChar(F_Pow(Convert.ToInt16(opentxt[i]), 2, n));
             }
             ShifrtxtBox.Text = shifr;
             //decrypt
-            DshifrtxtBox.Text = "1."+China_function(shifr, p, q, 1, 1);
+            DshifrtxtBox.Text = "1."+China_function(shifr, p, q, 1, 1)+"\n 2."+ China_function(shifr, p, q, 1, -1) + "\n 3." + China_function(shifr, p, q, -1, 1) + "\n 4." + China_function(shifr, p, q, -1, -1);
             
-            
-
         }
 
         bool prime(int n)
@@ -78,17 +76,14 @@ namespace Rabin_Cryptosystem
                 a0 = bufa;
                 b0 = bufb;
             }
-           /* if (a < 0)
+           if (a < 0)
                 return (a + x);
-            else*/
+            else
                 return (a);
         }
         static string China_function(string shifr, int p, int q, int xp, int yp)
         {
-            string dshifr1 = null;
-            string dshifr2 = null;
-            string dshifr3 = null;
-            string dshifr4 = null;
+            string dshifr = null;
             for (int i = 0; i < shifr.Length; i++)
             {
                 int k = (p + 1) / 4;
@@ -98,29 +93,22 @@ namespace Rabin_Cryptosystem
                 int y = F_Pow(Convert.ToInt16(shifr[i]), l, q);
                 x = x * xp;
                 y = y * yp;
-                int x1 = Mod(x, p);
-                int y1 = Mod(y, q);
-                int M1 = n / p;
-                int M2 = n / q;
+                while (x < 0)
+                    x = x + p;
+                while (y < 0)
+                    y = y + q;
+                x = x % p;
+                y = y % q;
+                int M1 = q;
+                int M2 = p;                
                 int M11 = Mod(M1, p);
                 int M21 = Mod(M2, q);
-                int msg1 = (x1 * p * M21 + y1 * q * M11) % n;
-                int msg2 = msg1 - n;
-                int msg3= (x1 * p * M21 - y1 * q * M11) % n;
-                int msg4 = msg3 - n;
-                while ((msg1 < 0) || (msg2 < 0) || (msg3 < 0) || (msg4 < 0))
-                {
-                    msg1 = msg1 + 255;
-                    msg2 = msg2 + 255;
-                    msg3 = msg3 + 255;
-                    msg4 = msg4 + 255;
-                }
-                dshifr1 = dshifr1 + Convert.ToChar(msg1 % 255);
-                dshifr2 = dshifr2 + Convert.ToChar(msg2 % 255);
-                dshifr3 = dshifr3 + Convert.ToChar(msg3 % 255);
-                dshifr4 = dshifr4 + Convert.ToChar(msg4 % 255);
+                int msg = (M1* M11 * x + M2 * M21 * y) % (p * q);
+               while (msg < 0)
+                    msg += 255;
+                dshifr = dshifr + Convert.ToChar(msg%256);
             }
-            return (dshifr1+dshifr2+dshifr3+dshifr4);
+            return (dshifr);
         }
     }
 
